256e8be3d836a1dba1ff4307ee3c422315fbc959
Raisa320 <67403486+Raisa320@users.noreply.github.com>
2022-10-06 02:51:47 -0500

    Mi primer commit, interfaz

---
+++ src/main/Main.java
@@ -0,0 +1,20 @@
+
+package main;
+
+import views.LoginView;
+
+/**
+ *
+ * @author Asus
+ */
+public class Main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+
+        /* Instacia del login */
+        LoginView login=new LoginView();
+        login.setVisible(true);
+    }
+}
